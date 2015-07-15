45945078c0a2eb88135dcf588967aa999b63e151
Margeaux Spring <contact@cheapwebmonkey.com>
2015-07-15 13:35:07 -0400

    nav adjustment to closed on mobile

--- js/hamnav.js
+++ js/hamnav.js
@@ -1,11 +1,11 @@
 $(function(){
-$( ".cross" ).hide();
-$( ".menu" ).hide();
-$( ".hamburger" ).click(function() {
-$( ".menu" ).slideToggle( "slow", function() {
-$( ".hamburger" ).hide();
-$( ".cross" ).show();
-});
+    $( ".cross" ).hide();
+    $( ".menu" ).hide();
+    $( ".hamburger" ).click(function() {
+    $( ".menu" ).slideToggle( "slow", function() {
+    $( ".hamburger" ).hide();
+    $( ".cross" ).show();
+  });
 });
 
 $( ".cross" ).click(function() {
